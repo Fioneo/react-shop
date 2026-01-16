ebdc80b2e4b62c35dde9966eeaf37f2f8e3e2116
Fioneo <max.kovalchuk.007@gmail.com>
2026-01-17 01:48:48 +0300

    fix screen height and add adaptive

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -4,14 +4,14 @@ import Footer from "./Footer";
 import { ScrollToTop } from "./ScrollToTop";
 function Layout() {
   return (
-    <>
+    <div className="app-container flex flex-col min-h-screen">
       <ScrollToTop />
       <Header />
-      <main className="bg-accent transition-colors duration-200">
+      <main className="bg-accent transition-colors duration-200 flex-1">
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
